b424a18e58264d45fe4c407bc0cd8191196b5ef8
Mahesh Galande <mgalande@saxon.ai>
2026-09-16 19:30:09 +0530

    changes

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-s =  "Mahesh"
-for right, char in enumerate(s): # (i , char)
-    print(right , char)
+def rain_water(heights: list) -> int:
+   left =0 
+   right=left+1
+   sum = 0
+
+   while right < len(heights): 
+      if heights[right] > heights[left]:
+        left = right
+        right+=1
+
+      sum += (heights[right]  - heights[left])
+      right +=1
+      left += 1
+   
+   return sum
+heights = [0,1,0,2,1,0,1,3,2,1,2,1]
+result = rain_water(heights)
+
+print(result)
